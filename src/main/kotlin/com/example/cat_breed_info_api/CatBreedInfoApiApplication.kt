ddc17e735da8dbf0286cc7f0162b31477785c5c8
Vinicius R <vrodrigues.code@gmail.com>
2024-07-21 16:53:49 -0300

    Spring initialzr

---
+++ src/main/kotlin/com/example/cat_breed_info_api/CatBreedInfoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cat_breed_info_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatBreedInfoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatBreedInfoApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/cat_breed_info_api/CatBreedInfoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cat_breed_info_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatBreedInfoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
